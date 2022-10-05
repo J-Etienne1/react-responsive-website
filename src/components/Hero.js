51dbde45c68991905aab4a864bbc84117b0d997f
Jason <jasonetienne1@gmail.com>
2022-10-05 19:26:42 +0100

    added hero, overlay and styling

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+const hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <p>Call us</p>
+            <p>1-800-DEAD</p>
+            <p>Beacuse Money</p>
+            <p>Doesn't come with Instructions</p>
+            <button href="/" className='button'>Free Consultation</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default hero
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, {useState} from "react";
+import { FaBars, FaTimes,FaGhost } from "react-icons/fa";
 import logo from "./images/logo.jpg";
-import "./Navbar.css"
-
+import "./Navbar.css";
 
 const Navbar = () => {
+const [click, setClick] = useState(false)
+
+const handleClick = () => setClick(!click)
+
   return (
     <div className="header">
       <nav className="navbar">
         <a href="/" className="logo">
           <img src={logo} alt="logo" />
         </a>
-        <div className="hamburger">
-          <FaBars />
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (<FaTimes size={30} style={{ color: "#ffffff" }}/>) : (<FaGhost size={30} style={{ color: "#ffffff" }} />)}
+          {/*FaGhost used instead of FaBars just for fun*/}
         </div>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active"  : "nav-menu"}>
           <li className="nav-item">
             <a href="/">Home</a>
           </li>
