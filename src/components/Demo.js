e0f2a74056065aadadf63f7fb1c30e491250f0b1
Jason <jasonetienne1@gmail.com>
2022-10-06 20:19:31 +0100

    demo and styling done

---
+++ src/components/Demo.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Demo.css";
+
+const Demo = () => {
+  return (
+    <div className="demo">
+      <div className="container">
+        <div className="col-1">
+          <p>Let's turn on the juice and see what shakes loose.</p>
+          <p>
+            These aren't my rules. Come to think of it, I don't have any rules.
+          </p>
+          <p>
+            What's The Good Of Being A Ghost If You Can't Frighten People Away?
+          </p>
+          <button className="button">Get your FREE Consultation</button>
+        </div>
+        <div className="col-2"></div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/wLw3SVzu4rk?clip=UgkxYGE5M7VmWn0xjLSci_WqTtxeZyOqaTHA&amp;clipt=EIPlCxjf1w0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
