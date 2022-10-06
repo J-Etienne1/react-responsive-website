583c8c3d644aa8247fe7d352e3b814b5ffb36ae1
Jason <jasonetienne1@gmail.com>
2022-10-06 19:53:32 +0100

    testimonials and styling done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const hero = () => {
   return (
-    <div className='hero'>
-        <div className='content'>
-            <p>Call us</p>
-            <p>1-800-DEAD</p>
-            <p>I'm the ghost with the most babe.</p>
-            <p>Never trust the living!</p>
-            <button href="/" className='button'>Free Consultation</button>
-
-        </div>
-
+    <div className="hero">
+      <div className="content">
+        <p>Call us</p>
+        <p>1-800-DEAD</p>
+        <p>I'm the ghost with the most babe.</p>
+        <p>Bio-Exorcist Extraordinaire</p>
+        <button href="/" className="button">
+          Free Consultation
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default hero;
---
+++ src/components/Testimonials.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Lid from "./images/Lid.jpg";
+import tes1 from "./images/tes1.jpg";
+import tes2 from "./images/tes2.jpg";
+import "./Testimonials.css";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonals">
+      <div className="container">
+        <h2>Testimonials</h2>
+        <span className="line"></span>
+        <div className="content">
+          <div className="card">
+            <img src={Lid} alt="Lidia" />
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+            <p>
+              <span>Lydia Deetz</span>
+            </p>
+            <p>I wanna be dead too.</p>
+          </div>
+          <div className="card">
+            <img src={tes1} alt="Lidia" />
+            <p>
+              sf ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+            <p>
+              <span>Barbara Maitland</span>
+            </p>
+            <p> Recently Deceased</p>
+          </div>
+          <div className="card">
+            <img src={tes2} alt="Lidia" />
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+            <p>
+              <span>Adam Maitland</span>
+            </p>
+            <p> Recently Deceased</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
